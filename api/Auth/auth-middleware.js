4b4d9ec0a7d528e22c085aed016ced627026024b
Canberk Ok <canberk.ok@gmail.com>
2023-07-21 14:06:10 +0300

    token

--- api/Auth/auth-middleware.js
+++ api/Auth/auth-middleware.js
@@ -1,10 +1,46 @@
-const resricted = (req,res,next) => {
- next({  // DON'T FORGET TO DELETE
-    status: 400,
-    message:"error middleware is OK..." ,
- })
-};
+const jwt = require('jsonwebtoken')
+const {JWT_SECRET} = require('../../config')
+const User = require('../Users/users-model');
+
+const resricted = async (req,res,next) => {
+    try {
+        const token =req.header.authorization;
+        if(token){
+            const tokenValue = await client.get(token);
+            if(tokenValue) {
+                jwt.verify(token, JWT_SECRET, (err,decodedJWT)=>{
+                    if(!err){
+                        req.decodedJWT = decodedJWT;
+                        next();
+                    } else {
+                        next(err);
+                    }
+                })
+            } else {
+                next({status:403, message: "Token is expired!.."})
+            }
+            
+        } else {
+            next({status:400, message: "Token required!.."})
+        }
+    } catch(err) {
+        next(err)
+    }
+}
+
+const generateToken = (req,res,next) => {
+    const payload = {
+        id: user_id,
+        username: username,
+    }
+    const options = {
+        expiresIn:"3h"
+    }
+    const token = jwt.sign(payload,JWT_SECRET,options)
+   };
+   
 
 module.exports = {
     resricted,
+    generateToken,
 }
